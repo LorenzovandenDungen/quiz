4306b06f19093f9303f78de0b5ed45adf5da5626
LorenzovandenDungen <2130766@talnet.nl>
2024-01-31 15:53:40 +0100

    save

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=world',
+    'dsn' => 'mysql:host=localhost;dbname=quiz',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
